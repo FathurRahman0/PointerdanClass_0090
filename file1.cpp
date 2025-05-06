0004128a6c48d872dbffc370d704e9a208b52ed0
FathurRahman0 <rahmanfthr165@gmail.com>
2025-05-06 16:18:48 +0700

    membuat insert persegi panajang

---
+++ file1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: // akses modifier
+    int panjang, lebar;   
+
+public:
+
+    void input()
+    {
+        cout << "Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+    
+    Lingkaran ol;
+
+};
+
+class Lingkaran
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin  >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
+
+int main ()
+{
+    PersegiPanjang objekPP; // membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> objekPP.lebar;
+    cout << "Luasa Persegi panjang : " <<
+    objekPP.luasPp(objekPP.panjang, objekPP.lebar ) << endl;
+
+
+
+}
--- file2.cpp
+++ file2.cpp
@@ -10,4 +10,19 @@ class mahasiswa{
 };
 
 
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); // member acces operator
+
+    mahasiswa &refMhs = mhs; // pointer reference refMhs
+    refMhs.nim == 2; // memeber acces operator
+    mhs.showNim();
+    
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
+
+
     
